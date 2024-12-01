db86bdb0645bb9632b5baf026af4a6c8f3518531
Hafsa <hafsajabeen47@gmail.com>
2024-12-01 11:36:14 +0500

    Code: star folder

---
+++ star/main.js
@@ -0,0 +1,64 @@
+
+for(let i=0; i<=10; i++){
+    
+    for(a=0; a<=i; a++){
+        let arr = ['*', '*', '*', '*', '*', '*', '*', '*', '*', '*',];
+        arr.pop();
+        document.write('*');
+        console.log(arr);
+       
+    }
+
+    document.write('</br>')
+};
+
+document.write('</br>');
+
+for(let i=0; i<=10; i++){
+    
+    for(a=10; a>=i; a--){
+        let arr = ['*', '*', '*', '*', '*', '*', '*', '*', '*', '*',];
+        arr.pop();
+        document.write('*');
+        console.log(arr);
+       
+    }
+
+    document.write('</br>')
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for(let i=10; i>=0; i--){
+    
+//     for(a=10; a>=i; a--){
+//         document.write('*')
+//         console.log(a)
+//     }
+
+//     document.write("</br>")
+// };
